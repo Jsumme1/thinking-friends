67a9e5231843267ebf2828930abefafafc29f91f
Jsumme1 <88805050+Jsumme1@users.noreply.github.com>
2021-12-17 15:32:38 -0600

    models created

--- models/thought.js
+++ models/thought.js
@@ -1,19 +1,20 @@
 const { Schema, model, Types } = require("mongoose");
-const dateFormat = require("../utils/dateFormat");
 
-const ReplySchema = new Schema(
+const ReactionSchema = new Schema(
   {
     // set custom id to avoid confusion with parent comment's _id field
-    replyId: {
+    replyReactionId: {
       type: Schema.Types.ObjectId,
       default: () => new Types.ObjectId(),
     },
-    replyBody: {
+    reactionBody: {
       type: String,
       required: true,
-      trim: true
+      trim: true,
+      max: 280,
     },
-    writtenBy: {
+
+    username: {
       type: String,
       required: true,
     },
@@ -30,23 +31,27 @@ const ReplySchema = new Schema(
   }
 );
 
-const CommentSchema = new Schema(
+const ThoughtSchema = new Schema(
   {
-    writtenBy: {
+    thoughtText: {
+      type: String,
+      required: true,
+      min: 1,
+      max: 280,
+    },
+
+    username: {
       type: String,
       required: true,
     },
-    commentBody: {
-      type: String,
-      required: true,
-    },
+
     createdAt: {
       type: Date,
       default: Date.now,
       get: (createdAtVal) => dateFormat(createdAtVal),
     },
     // use ReplySchema to validate data for a reply
-    replies: [ReplySchema],
+    reactions: [ReactionSchema],
   },
   {
     toJSON: {
@@ -57,10 +62,10 @@ const CommentSchema = new Schema(
   }
 );
 
-CommentSchema.virtual("replyCount").get(function () {
-  return this.replies.length;
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
 });
 
-const Comment = model("Comment", CommentSchema);
+const Thought = model("Thought", ThoughtSchema);
 
-module.exports = Comment;
+module.exports = Thought;
